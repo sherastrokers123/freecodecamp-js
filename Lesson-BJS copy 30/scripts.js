489d9896cba0d14f3a4af9e86c4da0ee6a8f2f2e
Cessabit <sherastrokers123@yandex.ru>
2022-11-08 20:19:45 +0300

    complete !))

---
+++ Lesson-BJS copy 30/scripts.js
@@ -0,0 +1,4 @@
+// Only change code below this line
+const myName = "Alisher";
+const myStr = "My name is " + myName + " and I am well!";
+console.log(myStr);
---
+++ Lesson-BJS copy 31/scripts.js
@@ -0,0 +1,5 @@
+// Change code below this line
+const someAdjective = "JS and ReactJS";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
+console.log(myStr);
