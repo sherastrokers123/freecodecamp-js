31bfb90b3099a551f363b35a6abbc30c7f2f738e
Cessabit <sherastrokers123@yandex.ru>
2022-11-09 13:03:24 +0300

    update
    new tasks solved

---
+++ Lesson-BJS copy 67/scripts.js
@@ -0,0 +1,13 @@
+function testLessThan(val) {
+    if (val) {  // Change this line
+        return "Under 25";
+    }
+
+    if (val) {  // Change this line
+        return "Under 55";
+    }
+
+    return "55 or Over";
+}
+
+testLessThan(10);
